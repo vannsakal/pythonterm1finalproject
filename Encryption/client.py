228b944af5aaa488dac9e9e3bbcd6ffa0afc6a90
PheakTra000 <tralegendary99@gmail.com>
2025-11-30 14:40:36 +0700

    encryption part

---
+++ Encryption/client.py
@@ -0,0 +1,22 @@
+import socket 
+import ssl
+
+HOST = 'localhost'
+PORT = 12345
+
+context = ssl.SSLContext(ssl.PROTOCOL_TLS_CLIENT)
+context.load_verify_locations(cafile='server.crt')
+context.check_hostname = False
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
+    try:
+        with context.wrap_socket(sock, server_hostname=HOST) as ssock:
+            ssock.connect((HOST, PORT))
+            print(f'Connected to {HOST} : {PORT}')
+            ssock.sendall(b'Hello from client securely!')
+            data = ssock.recv(1024)
+            print(f'Received from server: {data.decode()}')
+    except ssl.SSLError as e:
+        print(f'SSL Error: {e}')
+    finally:
+        sock.close()
---
+++ Encryption/server.py
@@ -0,0 +1,28 @@
+import socket 
+import ssl
+
+HOST = 'localhost'
+PORT = 12345
+
+context = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER)
+context.load_cert_chain(certfile='server.crt', keyfile='server.key')
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
+    sock.bind((HOST, PORT))
+    sock.listen(5)
+    print(f'Server listening on {HOST} : {PORT}')
+
+    while True:
+        conn, addr = sock.accept()
+        print(f'Accepted connection from {addr}')
+        
+        try:
+            with context.wrap_socket(conn, server_side=True) as ssock:
+                data = ssock.recv(1024)
+                print(f'Received from client: {data.decode()}')
+                ssock.sendall(b'Hello from server securely!')
+
+        except ssl.SSLError as e:
+            print(f'SSL Error: {e}')
+        finally:
+            conn.close()
